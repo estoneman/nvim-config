227db00d0771c4d02d5bbfc0b3d90b62659e6f43
estoneman <elstoneman11@gmail.com>
2024-09-22 16:43:41 -0500

    cleanup and add lazy home menu keymap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,18 +1,25 @@
 -- general
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- telescope.nvim
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {
-  desc = 'builtin.find_files: Lists files in your current working directory, respects .gitignore'
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+    desc = "builtin.find_files: Lists files in your current working directory, respects .gitignore"
 })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {
-  desc = 'builtin.live_grep: Fuzzy search through the output of git ls-files command, respects .gitignore'
+
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+    desc = "builtin.live_grep: Fuzzy search through the output of git ls-files command, respects .gitignore"
 })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {
-  desc = 'builtin.buffers: Searches for the string under your cursor or selection in your current working directory'
+
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+    desc = "builtin.buffers: Searches for the string under your cursor or selection in your current working directory"
 })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {
-  desc = 'builtin.help_tags: Search for a string in your current working directory and get results live as you type, respects .gitignore. (Requires ripgrep)'
+
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
+    desc = "builtin.help_tags: Search for a string in your current working directory and get results live as you type, respects .gitignore. (Requires ripgrep)"
+})
+
+vim.keymap.set("n", "<leader>lz", Lazy.home, {
+    desc = ":Lazy home: go to Lazy vim\"s home menu"
 })
