6509f932f4c6e3e1c2e94b31d72482a4b6ce482f
estoneman <elstoneman11@gmail.com>
2024-09-24 16:05:21 -0500

    rework structure

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,84 @@
+local on_attach = function(_, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+end
+
+local mason_lspconfig_setup = function()
+    local lspconfig = require("lspconfig")
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_lsp.default_capabilities()
+    )
+
+    return {
+        ensure_installed = {
+            "bashls",
+            "lua_ls",
+            "gopls",
+            "rust_analyzer",
+        },
+
+        automatic_installation = false,
+
+        handlers = {
+            ["lua_ls"] = function()
+                lspconfig["lua_ls"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = { "vim", "autocmd", },
+                            }
+                        }
+                    },
+                })
+            end,
+            ["rust_analyzer"] = function()
+                lspconfig["rust_analyzer"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                    settings = {
+                        diagnostics = {
+                            enable = false,
+                        },
+                    },
+                })
+            end,
+            ["gopls"] = function()
+                lspconfig["gopls"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                    settings = {
+                        gopls = {
+                            analyses = {
+                                unusedparams = true,
+                            },
+                            staticcheck = true,
+                            gofumpt = true,
+                        },
+                    },
+                })
+            end,
+            ["pylsp"] = function()
+                lspconfig["pylsp"].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+            function(server_name)
+                lspconfig[server_name].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end,
+        },
+    }
+end
+
 return {
     "neovim/nvim-lspconfig",
 
@@ -12,20 +93,21 @@ return {
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
+        "rafamadriz/friendly-snippets",
     },
 
-    config = function()
-        local lspconfig = require("lspconfig")
-        local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-        "force",
-        {},
-        vim.lsp.protocol.make_client_capabilities(),
-        cmp_lsp.default_capabilities()
-        )
+    config = function ()
+        require("mason").setup({})
+        require("mason-lspconfig").setup(mason_lspconfig_setup())
+        require("fidget").setup({})
 
+        local cmp = require("cmp")
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local has_words_before = function()
+            if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+            local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+        end
 
         cmp.setup({
             snippet = {
@@ -38,61 +120,20 @@ return {
                 ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
                 ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
+                ["<Tab>"] = vim.schedule_wrap(function(fallback)
+                    if cmp.visible() and has_words_before() then
+                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                    else
+                        fallback()
+                    end
+                end),
             }),
             sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-            }, {
-                { name = "buffer" },
-            })
-        })
-
-        require("fidget").setup({})
-        require("mason").setup({})
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "bashls",
-                "lua_ls",
-                "gopls",
-                "rust_analyzer",
-            },
-
-            automatic_installation = true,
-
-            handlers = {
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-                ["lua_ls"] = function()
-                    lspconfig["lua_ls"].setup({
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = {
-                                        "vim",
-                                        "autocmd"
-                                    },
-                                },
-                            },
-                        },
-                    })
-                end,
-                ["gopls"] = function()
-                    lspconfig["gopls"].setup({
-                        settings = {
-                            gopls = {
-                                analyses = {
-                                    unusedparams = true,
-                                },
-                                staticcheck = true,
-                                gofumpt = true,
-                            },
-                        },
-                    })
-                end,
-            },
+                { name = "nvim_lsp", group_index = 2},
+                { name = "luasnip", group_index = 2},
+                { name = "friendly-snippets", group_index = 2},
+            }),
+            { { name = "buffer" }, },
         })
     end,
 }
