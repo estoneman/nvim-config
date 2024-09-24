ba3fa2b08a95bbfda03c87c956859fe110cc247c
estoneman <elstoneman11@gmail.com>
2024-09-24 05:26:19 -0500

    add frecency extension to Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,17 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {},
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-frecency.nvim",
+    },
+    opts = {
+        extensions = {
+            frecency = {
+                auto_validate = false,
+                matcher = "fuzzy",
+                path_display = { "filename_first" },
+            },
+      },
+  }
 }
