15297851815696817ce6f6928bf2d623de8f91a2
estoneman <elstoneman11@gmail.com>
2024-10-02 20:31:09 -0500

    restructure for more generic colorscheme config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,74 +1,67 @@
--- kanagawa
-local kanagawa = {
-    "rebelot/kanagawa.nvim",
+local colorscheme = {
     lazy = false,
-    init = function()
-        vim.cmd("colorscheme kanagawa-dragon")
-    end,
     priority = 1000,
-    name = "kanagawa",
-    opts = {
-        commentStyle = {
-            italic = false
-        },
-        keywordStyle = {
-            italic = false
-        },
-        colors = {
-            theme = {
-                all = {
-                    ui = {
-                        bg_gutter = "none"
+    kanagawa = {
+        "rebelot/kanagawa.nvim",
+        init = function()
+            vim.cmd("colorscheme kanagawa-dragon")
+        end,
+        name = "kanagawa",
+        opts = {
+            commentStyle = {
+                italic = false
+            },
+            keywordStyle = {
+                italic = false
+            },
+            theme = "dragon",
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none"
+                        }
                     }
                 }
-            }
+            },
+            overrides = function(colors)
+                local theme = colors.theme
+
+                return {
+                    String = {
+                        bg = nil,
+                        fg = colors.palette.springGreen,
+                        italic = false,
+                    },
+                    Function = {
+                        bg = nil,
+                        fg = colors.palette.carpYellow,
+                    },
+
+                    NormalFloat = { bg = "none" },
+                    FloatBorder = { bg = "none" },
+                    FloatTitle = { bg = "none" },
+
+                    -- Save an hlgroup with dark background and dimmed foreground
+                    -- so that you can use it where your still want darker windows.
+                    -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+                    -- Popular plugins that open floats will link to NormalFloat by default;
+                    -- set their background accordingly if you wish to keep them dark and borderless
+                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                }
+            end,
         },
-        overrides = function(colors)
-            local theme = colors.theme
-
-            return {
-                String = {
-                    bg = nil,
-                    fg = colors.palette.springGreen,
-                    italic = false,
-                },
-                Function = {
-                    bg = nil,
-                    fg = colors.palette.carpYellow,
-                },
-
-                NormalFloat = { bg = "none" },
-                FloatBorder = { bg = "none" },
-                FloatTitle = { bg = "none" },
-
-                -- Save an hlgroup with dark background and dimmed foreground
-                -- so that you can use it where your still want darker windows.
-                -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-                NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-                -- Popular plugins that open floats will link to NormalFloat by default;
-                -- set their background accordingly if you wish to keep them dark and borderless
-                LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            }
+    },
+    rose_pine = {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        init = function()
+            vim.cmd("colorscheme rose-pine")
         end,
     },
 }
 
--- rose pine
-local _ = {
-    "rose-pine/neovim",
-    lazy = false,
-    priority = 1000,
-    init = function()
-        vim.cmd("colorscheme rose-pine")
-    end,
-    name = "rose-pine",
-    opts = {
-        styles = {
-            italic = false,
-        },
-    },
-}
-
-return kanagawa
+return colorscheme["kanagawa"]
