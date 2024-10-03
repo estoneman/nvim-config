44d66b27f73926d023f27d9c35106cf39eaa0a97
estoneman <elstoneman11@gmail.com>
2024-10-02 20:31:23 -0500

    add dashboard w/ initial config

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,87 @@
+local conf = {}
+
+conf.header = {
+    "                              ",
+    "                              ",
+    "                              ",
+    "                        'c.   ",
+    "                 ,xNMM.       ",
+    "               .OMMMMo        ",
+    "               OMMM0,         ",
+    "     .;loddo:' loolloddol;.   ",
+    "   cKMMMMMMMMMMNWMMMMMMMMMM0: ",
+    " .KMMMMMMMMMMMMMMMMMMMMMMMWd. ",
+    " XMMMMMMMMMMMMMMMMMMMMMMMX.   ",
+    ";MMMMMMMMMMMMMMMMMMMMMMMM:    ",
+    ":MMMMMMMMMMMMMMMMMMMMMMMM:    ",
+    ".MMMMMMMMMMMMMMMMMMMMMMMMX.   ",
+    " kMMMMMMMMMMMMMMMMMMMMMMMMWd. ",
+    " .XMMMMMMMMMMMMMMMMMMMMMMMMMMk",
+    "  .XMMMMMMMMMMMMMMMMMMMMMMMMK.",
+    "    kMMMMMMMMMMMMMMMMMMMMMMd  ",
+    "     ;KMMMMMMMWXXWMMMMMMMk.   ",
+    "       .cooc,.    .,coo:.     ",
+    "                              ",
+    "                              ",
+    "                              ",
+}
+
+conf.center = {
+  {
+    icon = "󰈞  ",
+    desc = "Find  File                              ",
+    action = "Leaderf file --popup",
+    key = "<Leader> f f",
+  },
+  {
+    icon = "󰈢  ",
+    desc = "Recently opened files                   ",
+    action = "Leaderf mru --popup",
+    key = "<Leader> f r",
+  },
+  {
+    icon = "󰈬  ",
+    desc = "Project grep                            ",
+    action = "Leaderf rg --popup",
+    key = "<Leader> f g",
+  },
+  {
+    icon = "  ",
+    desc = "Open Nvim config                        ",
+    action = "tabnew $MYVIMRC | tcd %:p:h",
+    key = "<Leader> e v",
+  },
+  {
+    icon = "  ",
+    desc = "New file                                ",
+    action = "enew",
+    key = "e",
+  },
+  {
+    icon = "󰗼  ",
+    desc = "Quit Nvim                               ",
+    action = "qa",
+    key = "q",
+  },
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "dashboard",
+  group = vim.api.nvim_create_augroup("dashboard_enter", { clear = true }),
+  callback = function ()
+    vim.keymap.set("n", "q", ":qa<CR>", { buffer = true, silent = true })
+    vim.keymap.set("n", "e", ":enew<CR>", { buffer = true, silent = true })
+  end
+})
+
+return {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+        require("dashboard").setup({
+            theme = 'doom',
+            config = conf,
+        })
+    end,
+    dependencies = { {"nvim-tree/nvim-web-devicons"} }
+}
